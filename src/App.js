0e19ada468a74adca81e7480e2f6a2e999ba23a2
cryger <henrygarcia-15@hotmail.com>
2023-02-20 01:47:20 -0500

    social network app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css"
+function App() {
+  return (
+    <div className="App">
+        <div className="blur">
+
+        </div>
+        <div className="blur"></div>
+    </div>
+  );
+}
+
+export default App;
