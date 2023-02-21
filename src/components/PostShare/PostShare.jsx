3d6d4a1f0b6bbe6b6cceabba752e3bf14f47d857
cryger <henrygarcia-15@hotmail.com>
2023-02-21 09:43:15 -0500

    agregados nuevos componentes al proyecto

--- src/components/PostShare/PostShare.jsx
+++ src/components/PostShare/PostShare.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 import ProfileImage from '../../img/profileImg.jpg'
 import './PostShare.css'
-import {UilScenery,UilPlayCircle,UilLocationPoint,UilSchedule} from "@iconscout/react-unicons"
+import {UilScenery,UilPlayCircle,UilLocationPoint,UilSchedule, UilTimes} from "@iconscout/react-unicons"
 
 export const PostShare = () => {
+    const [image, setImage] = useState(null);
+    const imageRef = useRef();
+
+    const onImageChange = (event) =>{
+        if(event.target.files && EventTarget.target.files[0]){
+            let img = event.target.files[0];
+            setImage({
+                image: URL.createObjectURL(img),
+            });
+        }
+    }
   return (
     <div className="PostShare">
         <img src={ProfileImage} alt="" />
             <div>
                 <input type="text"  placeholder="What's Happening "/>
                 <div className="PostOptions">
-                    <div className="options" style={{color: "var(--photo"}}>
+                    <div className="options" style={{color: "var(--photo"}}
+                    onClick={()=>imageRef.current.click()}
+                    >
                         <UilScenery/>
                             
                             Photo
@@ -34,7 +47,16 @@ export const PostShare = () => {
                         
                     </div>
                     <button className='btnFollow ps-button'>Share</button>
+                    <div style={{display:"none"}}>
+                        <input type="file" name='myImage' ref={imageRef} onChange={onImageChange}/>
+                    </div>
                 </div>
+                {image &&
+                    <div className="previewImage">
+                        <UilTimes onClick={()=>setImage(null)}/>
+                        <img src={image.image} alt="" />
+                    </div>
+                }
             </div>
     </div>
     
